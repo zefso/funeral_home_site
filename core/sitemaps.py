2cee0df77d60f43b465a13bb880a35ce2f15bce8
zefso <roman27gorbach@gmail.com>
2026-02-20 12:01:50 +0100

    SEO: enable i18n and lastmod in sitemap

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -5,9 +5,14 @@ class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = 'daily'
     protocol = 'https'
+    i18n = True
 
     def items(self):
         return ['core:home', 'core:service_list', 'core:precaution', 'core:funeral_speeches', 'core:farewell', 'core:contact']
 
     def location(self, item):
         return reverse(item)
+
+    def lastmod(self, item):
+        from django.utils import timezone
+        return timezone.now()
